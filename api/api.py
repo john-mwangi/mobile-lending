653633d84cb2875f68fb2a4ca01b829bbaea973b
John Mwangi <mail.mwangi@gmail.com>
2022-02-09 00:49:48 +0300

    Added default json response.

--- api/api.py
+++ api/api.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from .utils import Loader
 from functools import lru_cache
+from .utils import Scorer
+import pandas as pd
 
-SAMPLES = 5
+SAMPLES = 2
 
 app = FastAPI()
 
@@ -10,24 +11,32 @@ app = FastAPI()
 # uvicorn api.api:app --host 0.0.0.0 --port 12000 --reload
 # localhost:12000/docs for Swagger documentation & testing
 
+sc = Scorer()
+
+train_X = sc.load_test_data(items=["train_X"])
+train_X = train_X[0]
+train_X = train_X.sample(n=SAMPLES)
+train_X_json = train_X.to_json()
+
 
 @app.get(path="/")
 def read_root():
     return {"Test": "Fast API ran successfully!!"}
 
 
-@app.get(path="/predict")
 @lru_cache(maxsize=128)
-def fetch_predictions():
-    dr = Loader()
+@app.get(path="/predict")
+def fetch_predictions(resp=train_X_json):
 
-    train_X, op_thr = dr.load_test_data()
-    default_preds, adjusted_preds = dr.obtain_preds(
+    train_X = pd.read_json(resp)
+    op_thr = sc.load_test_data(items=["op_thr"])
+
+    default_preds, adjusted_preds = sc.obtain_preds(
         data=train_X.sample(n=SAMPLES), op_thr=op_thr
     )
 
     return {
-        "Loaded models": len(dr.load_models()),
+        "Loaded models": len(sc.load_models()),
         "Train X": train_X.shape,
         "Default preds": default_preds,
         "Adjusted preds": adjusted_preds,
