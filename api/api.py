d97b3414c834cc13f8737141fe6ce85123e7e592
John Mwangi <mail.mwangi@gmail.com>
2022-02-11 16:18:03 +0300

    Minor update: - Exception handling - API descriptions
    .

--- api/api.py
+++ api/api.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from functools import lru_cache
 from .utils import Scorer
 import pandas as pd
 
 SAMPLES = 2
 
-app = FastAPI()
+app = FastAPI(title="Mobile Lending Fast API")
 
 # To run this API on the terminal...
 # uvicorn api.api:app --host 0.0.0.0 --port 12000 --reload
@@ -26,14 +26,22 @@ def read_root():
 
 @lru_cache(maxsize=128)
 @app.get(path="/predict")
-def fetch_predictions(resp=train_X_json):
+def fetch_predictions(
+    resp: str = Query(
+        default=train_X_json,
+        description="Customer details obtained after data merging and preparation.",
+    )
+) -> dict:
 
     train_X = pd.read_json(resp)
     op_thr = sc.load_test_data(items=["op_thr"])
 
-    default_preds, adjusted_preds = sc.obtain_preds(
-        data=train_X.sample(n=SAMPLES), op_thr=op_thr
-    )
+    try:
+        default_preds, adjusted_preds = sc.obtain_preds(
+            data=train_X.sample(n=SAMPLES), op_thr=op_thr
+        )
+    except Exception as e:
+        default_preds, adjusted_preds = ["An error has occured"] * 2
 
     return {
         "Train X": train_X.shape,
